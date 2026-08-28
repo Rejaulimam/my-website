59d13246fc58cc71e7c3b4b801134bf0da20c1bc
Rejaulimam <rejaul.karim.bdnet1@gmail.com>
2026-08-28 12:23:45 +0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = "rk-store-v1";
+
+const APP_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(APP_FILES))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+
+                const responseClone =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(
+                            event.request,
+                            responseClone
+                        );
+                    });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
